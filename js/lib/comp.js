fcebd1190811d470c06338b03a46eb6d6c81e4cf
mtogneri <mtogneri@openwt.com>
2019-02-17 19:21:37 +0100

    Improved JS version

--- js/lib/comp.js
+++ js/lib/comp.js
@@ -5,3 +5,43 @@ function Equal(a, b)
 		return a.eval() === b.eval()
 	}
 }
+
+function NotEqual(a, b)
+{
+	this.eval = function()
+	{
+		return a.eval() !== b.eval()
+	}
+}
+
+function LessThan(a, b)
+{
+	this.eval = function()
+	{
+		return a.eval() < b.eval()
+	}
+}
+
+function LessEqual(a, b)
+{
+	this.eval = function()
+	{
+		return a.eval() <= b.eval()
+	}
+}
+
+function GreaterThan(a, b)
+{
+	this.eval = function()
+	{
+		return a.eval() > b.eval()
+	}
+}
+
+function GreaterEqual(a, b)
+{
+	this.eval = function()
+	{
+		return a.eval() >= b.eval()
+	}
+}
