eb32062ab07c496963ea322bdf5461ce5c336294
mtogneri <mtogneri@openwt.com>
2019-02-17 19:27:20 +0100

    Improved JS version

--- js/lib/math.js
+++ js/lib/math.js
@@ -1,8 +1,8 @@
-function Mul(a, b)
+function Add(a, b)
 {
 	this.eval = function()
 	{
-		return a.eval() * b.eval()
+		return a.eval() + b.eval()
 	}
 }
 
@@ -11,5 +11,53 @@ function Sub(a, b)
 	this.eval = function()
 	{
 		return a.eval() - b.eval()
+	}
+}
+
+function Mul(a, b)
+{
+	this.eval = function()
+	{
+		return a.eval() * b.eval()
+	}
+}
+
+function Div(a, b)
+{
+	this.eval = function()
+	{
+		return a.eval() / b.eval()
+	}
+}
+
+function Inc(a)
+{
+	this.eval = function()
+	{
+		return a.eval() + 1
+	}
+}
+
+function Dec(a)
+{
+	this.eval = function()
+	{
+		return a.eval() - 1
+	}
+}
+
+function Pow(a, b)
+{
+	this.eval = function()
+	{
+		return Math.pow(a.eval(), b.eval())
+	}
+}
+
+function Mod(a, b)
+{
+	this.eval = function()
+	{
+		return a.eval() % b.eval()
 	}
 }
